f2ad5b2127f4733bacf69931913018cac2042059
jalal-lagdimi <jalagdimi@gmail.com>
2022-11-17 16:53:08 +0100

    adding backend to front end

---
+++ statistic.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NFTea</title>
+    <link rel="stylesheet" href="Collection.css" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <script defer src="main.js"></script>
+    <script defer src="./logic.js"></script>
+
+</head>
+<body>
+      <!--header--->
+      <header>
+        <a href="index.html" class="logo"><img src="./img/LOGO" alt="logo"></a>
+            <ul class="NAVBAR">
+            <i class="bi bi-x-circle clo-ico" id="close"></i>
+                 <li> <a href="index.php">HOME</a> </li>
+                 <li> <a href="#collection">COLLECTION</a> </li>
+                 <li> <a href="#">STATISTICS</a> </li> 
+            </ul>
+            <i class="bi bi-list resp-ico" id="open"></i>
+            
+    </header>
+
+
+     <!-- foooter -->
+     <footer >
+        <section class="footer">
+            <div class="footer-content">
+                <div>
+                    <H3>NFTea</H3>
+                    <p>Lorem ipsum dolor sit amet adipisicing elit. <br> Lorem ipsum dolor sit amet adipisicing. <br> Aliquam sapiente ipsum suscipit  <br> Sequi dicta.</p>
+                </div>
+                <div>
+                    <h3>Navigation</h3>
+                    <ul>
+                        <li>Home</li>
+                        <li>Collection</li>
+                        <li>Statistics</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Quick link</h3>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Instagram</li>
+                        <li>Twitter</li>
+                    </ul>
+                </div>
+                <div>
+                    <img src="./img/footer-img.jpg" alt="">
+                </div>
+            </div>
+            <div class="copyright">
+                <p>© Copyright 2022 Desgined. All Rights Reserved.</p>
+            </div>  
+        </section> 
+    </footer>  
+</body>
+</html>
