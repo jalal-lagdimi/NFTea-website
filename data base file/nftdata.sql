8f674f488204aded4655fb14a34148d5475bf577
jalal-lagdimi <jalagdimi@gmail.com>
2022-11-19 15:59:03 +0100

    add data file

---
+++ data base file/nftdata.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : sam. 19 nov. 2022 à 13:07
+-- Version du serveur : 10.4.25-MariaDB
+-- Version de PHP : 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `nftdata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `collection`
+--
+
+CREATE TABLE `collection` (
+  `id` int(250) NOT NULL,
+  `nom` varchar(250) NOT NULL,
+  `artiste` varchar(250) NOT NULL,
+  `image` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `collection`
+--
+
+INSERT INTO `collection` (`id`, `nom`, `artiste`, `image`) VALUES
+(41, ' boura', 'bouraton', 'footer-img.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nft`
+--
+
+CREATE TABLE `nft` (
+  `id` int(250) NOT NULL,
+  `nom` varchar(250) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `prix` varchar(250) NOT NULL,
+  `image` varchar(250) NOT NULL,
+  `idcollection` int(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `collection`
+--
+ALTER TABLE `collection`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `nft`
+--
+ALTER TABLE `nft`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nft_ibfk_1` (`idcollection`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `collection`
+--
+ALTER TABLE `collection`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT pour la table `nft`
+--
+ALTER TABLE `nft`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `nft`
+--
+ALTER TABLE `nft`
+  ADD CONSTRAINT `nft_ibfk_1` FOREIGN KEY (`idcollection`) REFERENCES `collection` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
