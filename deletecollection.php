ca0dbfac330188ad59df1b3bb2e15fcaf8f74100
jalal-lagdimi <jalagdimi@gmail.com>
2022-11-24 13:30:37 +0100

    edit

--- deletecollection.php
+++ deletecollection.php
@@ -2,7 +2,7 @@
         include 'connect.php';
        
         $id =$_GET['id'];
-        $select=mysqli_query($conn,"DELETE FROM collection WHERE id = $id");
+        mysqli_query($conn,"DELETE FROM collection WHERE id = $id");
         header('Location:index.php');
        
 ?>
--- deletenft.php
+++ deletenft.php
@@ -3,7 +3,7 @@
         include 'connect.php';
         $idcoll =$_GET['idcoll'];
         $id =$_GET['id'];
-        $select=mysqli_query($conn,"DELETE FROM nft WHERE id = $id");
+        mysqli_query($conn,"DELETE FROM nft WHERE id = $id");
         header('Location:NFTs.php?id='.$idcoll);
        
 ?>
